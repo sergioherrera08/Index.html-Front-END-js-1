3e16f464706e599397c357739457cbff6b7e21ca
sergioherrera08 <hesergio0@gmail.com>
2026-07-06 14:15:07 -0300

    Delete pages directory

--- pages/js/contacto.js
+++
@@ -1,9 +0,0 @@
-contacto.js
-
-import { obtenerCarrito } from "./storage.js";
-import { actualizarContador } from "./ui.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    const carrito = obtenerCarrito();
-    actualizarContador(carrito);
-});
